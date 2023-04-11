b01b159c54919e2433cd3187e5fc9599fed7344a
Shourya Dwivedi <shouryad28@gmail.com>
2023-04-11 16:54:52 +0530

    add better priting

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ async def get_all_messages(request: Request, x_hub_signature: str = Header(None)
     try:
         payload = await request.body()
         decoded_payload = payload.decode("utf-8")
+        print(type(decoded_payload), decoded_payload)
         for key in decoded_payload.keys():
             print(key, payload[key])
         signature = generate_hash_signature(payload)
